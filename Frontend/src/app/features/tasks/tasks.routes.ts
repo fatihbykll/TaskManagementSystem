1250a5c97413a21eaa48b7dde7a3fce2ca4a8df6
fatihbykll <449498@ogr.ktu.edu.tr>
2026-07-30 19:35:39 +0300

    refactor(frontend/tasks): tasks.routes.ts'e görev detay (:id) route'u eklendi

--- Frontend/src/app/features/tasks/tasks.routes.ts
+++ Frontend/src/app/features/tasks/tasks.routes.ts
@@ -4,5 +4,10 @@ export const TASKS_ROUTES: Routes = [
     path: '',
     loadComponent: () =>
       import('./task-list/task-list.component').then(m => m.TaskListComponent)
+  },
+  {
+    path: ':id',
+    loadComponent: () =>
+      import('./task-detail/task-detail.component').then(m => m.TaskDetailComponent)
   }
 ];
